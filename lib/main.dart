9db631f8ce28e305fbd778c819f66d86bc486e5d
LoveSolaristics <lovesolaristics@gmail.com>
2021-08-19 22:10:02 +0300

    Add author page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:quizapp/pages/home.dart';
+import 'package:quizapp/pages/author.dart';
 
 void main() => runApp(MaterialApp(
   theme: ThemeData(
     primaryColor:  Colors.deepOrangeAccent,
   ),
-  home: Home(),
+  initialRoute: '/',
+  routes: {
+    '/': (context)=> Home(),
+    '/author': (context)=> Author(),
+  },
+
 ));
--- lib/pages/author.dart
+++ lib/pages/author.dart
@@ -1,1 +1,34 @@
 import 'package:flutter/material.dart';
+
+class Author extends StatelessWidget {
+  const Author({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text('Автор'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 70, left: 95),
+        child: Column(
+          children: [
+            Container(
+              child: CircleAvatar(backgroundImage: NetworkImage('https://avatars.githubusercontent.com/u/32870032?v=4')),
+              width: 160,
+              height: 160,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+              ),
+            ),
+            Text('Анфимов Дмитрий', style: TextStyle(fontSize: 25, height: 4),),
+            ElevatedButton(onPressed: (){}, child: Text('Github проекта'))
+          ],
+        ) ,
+      )
+    );
+  }
+}
+
